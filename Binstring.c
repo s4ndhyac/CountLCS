ee97c1b2360e61510f20824dbf81d4a80c5f4db6
s4ndhyac <s4ndhyac@gmail.com>
2018-12-01 16:18:49 -0800

    binstring

--- Binstring.c
+++ Binstring.c
@@ -2,4 +2,21 @@
 
 char *Binstring(int n, int x)
 {
+  char *r = (char *)malloc(n + 1);
+
+  int i, tmp;
+  int c = 0;
+  for (i = n - 1; i >= 0; i--)
+  {
+    tmp = x >> i;
+    if (tmp & 1)
+      *(r + c) = 1 + '0';
+    else
+      *(r + c) = 0 + '0';
+
+    c++;
+  }
+
+  *(r + c) = '\0';
+  return r;
 }
