88a45d5eff1f6c7184f7fb0c7fcb1c601df5e514
s4ndhyac <s4ndhyac@gmail.com>
2018-12-04 20:51:32 -0800

    Bonus attempt get x and y for most LCS count

---
+++ Bonus.cc
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "MaxLCS.cc"
+#include "Binstring.cc"
+
+using namespace std;
+
+int main()
+{
+  //Read input n
+  int n;
+  char term;
+  printf("Enter the value of n in range [3:20] ");
+  if (scanf("%d%c", &n, &term) != 2 || term != '\n')
+  {
+    printf("Valid integer not entered\n");
+    exit(0);
+  }
+  if (n < 3 || n > 20)
+  {
+    printf("Entered number is not in range\n");
+    exit(0);
+  }
+
+  // int max = pow(2, n) - 1;
+
+  // //Read input x
+  // int x;
+  // printf("Enter value of x in range [0:%d]: ", max);
+  // if (scanf("%d%c", &x, &term) != 2 || term != '\n')
+  // {
+  //   printf("Valid integer not entered\n");
+  //   exit(0);
+  // }
+  // if (x < 0 || x > max)
+  // {
+  //   printf("Entered number is not in range\n");
+  //   exit(0);
+  // }
+
+  // //Read input y
+  // int y;
+  // printf("Enter value of y in range [0:%d]: ", max);
+  // if (scanf("%d%c", &y, &term) != 2 || term != '\n')
+  // {
+  //   printf("Valid integer not entered\n");
+  //   exit(0);
+  // }
+  // if (y < 0 || y > max)
+  // {
+  //   printf("Entered number is not in range\n");
+  //   exit(0);
+  // }
+
+  int x = 0;
+  int y = 0;
+  int max = pow(2, n) - 1;
+  int maxCount = 0;
+  for (x = 0; x <= max; x++)
+  {
+    for (y = 0; y <= max; y++)
+    {
+      char *a = Binstring(n, x);
+      char *b = Binstring(n, y);
+      int count = GetCommomSubsequencesCount(a, b, n, n);
+      if (count > maxCount)
+      {
+        maxCount = count;
+        cout << "x: " << x << " y: " << y << " s: " << maxCount << endl;
+      }
+    }
+  }
+
+  // //Program execution starts
+  // auto start = std::chrono::high_resolution_clock::now();
+
+  // //Get binstring from n and x
+
+  // auto f1 = std::chrono::high_resolution_clock::now();
+  // auto m1 = std::chrono::duration_cast<std::chrono::microseconds>(f1 - start);
+  // cout << "X: " << m1.count() << "µs\n";
+
+  // //Get binstring from n and x
+
+  // auto f2 = std::chrono::high_resolution_clock::now();
+  // auto m2 = std::chrono::duration_cast<std::chrono::microseconds>(f2 - start);
+  // cout << "Y: " << m2.count() << "µs\n";
+
+  // //Get LCS
+  // set<string> lcs;
+  // int lcsLen = CountLCS(n, a, b, lcs);
+
+  // auto f3 = std::chrono::high_resolution_clock::now();
+  // auto m3 = std::chrono::duration_cast<std::chrono::microseconds>(f3 - start);
+  // cout << "CountLCS Total: " << m3.count() << "µs\n";
+
+  // int count = lcs.size();
+
+  // //output the results
+  // printf("STRINGLEN: %d\n", n);
+  // printf("X (%d) = %s\n", x, a);
+  // printf("Y (%d) = %s\n", y, b);
+  // printf("Length of LCS: %d\n", lcsLen);
+  // printf("No. of LCS: %d\n", count);
+
+  // int i = 1;
+  // set<string>::iterator it;
+  // for (it = lcs.begin(); it != lcs.end(); it++)
+  // {
+  //   cout << "LCS " << i++ << ": " << *it << endl;
+  // }
+
+  // //Program ends
+  // auto finish = std::chrono::high_resolution_clock::now();
+  // auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(finish - start);
+  // cout << "Total: " << microseconds.count() << "µs\n";
+
+  fflush(stdout);
+}
---
+++ MaxLCS.cc
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <stdio.h>
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+
+int **dp;
+int **memo;
+
+void LCSLength(const int n, char *a, char *b)
+{
+  memo = new int *[n + 1];
+  for (int i = 0; i < n + 1; ++i)
+    memo[i] = new int[n + 1];
+  int i, j;
+  for (i = 0; i <= n; i++)
+  {
+    for (j = 0; j <= n; j++)
+    {
+      if (i == 0 || j == 0)
+        memo[i][j] = 0;
+      else if (a[i - 1] == b[j - 1])
+        memo[i][j] = 1 + memo[i - 1][j - 1];
+      else
+        memo[i][j] = MAX(memo[i][j - 1], memo[i - 1][j]);
+    }
+  }
+}
+
+int GetCommomSubsequencesCount(char *s1, char *s2, const int N1, const int N2)
+{
+  LCSLength(N1, s1, s2);
+  dp = new int *[N1 + 1];
+  for (int i = 0; i < N1 + 1; ++i)
+    dp[i] = new int[N2 + 1];
+
+  // for (int i = 0; i <= N1; i++) //N1 is size of 1st string
+  // {
+  //   for (int j = 0; j <= N2; j++) //N2 is size of 2nd string
+  //   {
+  //     dp[i][j] = 0;
+  //   }
+  // }
+
+  for (int b = 0; b <= N2; b++)
+  {
+    for (int a = 0; a <= N1; a++)
+    {
+      if (a == 0 || b == 0)
+        dp[a][b] = 1;
+      else
+      {
+        dp[a][b] = 0;
+        if (s1[a] == s2[b])
+          dp[a][b] = dp[a - 1][b - 1];
+        else
+        {
+          if (memo[a - 1][b] == memo[a][b])
+            dp[a][b] = dp[a][b] + dp[a - 1][b];
+
+          if (memo[a][b - 1] == memo[a][b])
+            dp[a][b] = dp[a][b] + dp[a][b - 1];
+
+          if (memo[a - 1][b - 1] == memo[a][b])
+            dp[a][b] = dp[a][b] - dp[a - 1][b - 1];
+        }
+      }
+    }
+  }
+
+  //   for j ← 0 to n do
+  //     for i ← 0 to m do
+  //         if i = 0 or j = 0 then
+  //             D[i, j] ← 1
+  //         else
+  //             D[i, j] ← 0
+  //             if ai  = bj  then
+  //                 D[i, j] ← D[i − 1, j − 1]
+  //             else if L[i − 1, j] = L[i, j] then
+  //                 D[i, j] ← D[i, j] + D[i − 1, j]
+  //             endif
+  //             if L[i, j − 1] = L[i, j] then
+  //                 D[i, j] ← D[i, j] + D[i, j − 1]
+  //             endif
+  //             if L[i − 1, j − 1] = L[i, j] then
+  //                 D[i, j] ← D[i, j] − D[i − 1, j − 1]
+  //             endif
+  //         end if
+  //     endfor
+  // endfor
+
+  // for (int i = 1; i <= N1; i++)
+  // {
+  //   for (int j = 1; j <= N2; j++)
+  //   {
+  //     if (s1[i - 1] == s2[j - 1])
+  //     {
+  //       dp[i][j] = 1 + dp[i][j - 1] + dp[i - 1][j];
+  //     }
+  //     else
+  //     {
+  //       dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1];
+  //     }
+  //   }
+  // }
+
+  int max = dp[N1][N2];
+  delete dp;
+  return max;
+}
